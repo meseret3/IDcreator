03c07e8dd6d1581408ab4e445499850dd7748f4d
SamArsanios <samson.kibrom@hotmail.com>
2022-10-04 13:42:30 -0400

    Add method to modify font

--- main.js
+++ main.js
@@ -1,85 +1,105 @@
+import allFonts from "./fonts.js";
 
-var canvas = new fabric.Canvas("canvas1");
-//var fonts = ["Montesserat bold", "Montesserat", "Times New Roman", "SanSerif","Arial"];
-var fonts = ["Pacifico", "VT323", "Quicksand", " montB"];
+let canvas = new fabric.Canvas("canvas1");
+
 // Create a new instance of Canvas
-function Adjust(){
-var can = document.getElementById('canvas1');
-can.style.border="solid black";
-var context = can.getContext('2d');
-context.fillStyle ="black";
-var x = document.getElementById("width").value;
-var y = document.getElementById("height").value;
-xp = x/0.0264583333;
-yp=y/0.0264583333;
+function Adjust() {
+  document.getElementById("form").addEventListener("submit", (e) => {
+    e.preventDefault();
 
-canvas.setDimensions({width:xp,height:yp});
-canvas.clone(function(clonedCanvas) {
-    // Convert our cloned Canvas Object to JSON
-    var canvasJSON = clonedCanvas.toJSON();
-    // Load the new cloned Canvas Object to canvas
-    
- });
-}
+    let can = document.getElementById("canvas1");
+    can.style.border = "solid black";
+    let context = can.getContext("2d");
+    context.fillStyle = "black";
+    let x = document.getElementById("width").value;
+    let y = document.getElementById("height").value;
+    let xp = x / 0.0264583333;
+    let yp = y / 0.0264583333;
 
-function CreateText(){
-   // canvas.loadFromJSON(canvasJSON);
-    //var canvas1 = new fabric.Canvas('canvas')
-    var text = new fabric.Textbox('PutTextHere', {
-        fill: 'blue',
-        fontStyle: "bold",
-        fontFamily: "montB"
-         
-
-        
-        //fontFamily: 'montB'
+    canvas.setDimensions({ width: xp, height: yp });
+    canvas.clone(function (clonedCanvas) {
+      // Convert our cloned Canvas Object to JSON
+      const canvasJSON = clonedCanvas.toJSON();
+      // Load the new cloned Canvas Object to canvas
     });
-    canvas.add(text).setActiveObject(text);
-    fonts.unshift('Times New Roman');
+  });
 }
 
-document.getElementById('file').addEventListener("change", function (e) {
-    var file = e.target.files[0];
-    var reader = new FileReader();
-    reader.onload = function (f) {
-      var data = f.target.result;                    
-      fabric.Image.fromURL(data, function (img) {
-        var oImg = img.set({left: 40, top: 0, angle: 0,width:520, height:600}).scale(0.4);
-        canvas.add(oImg).renderAll();
-        var a = canvas.setActiveObject(oImg);
-        var dataURL = canvas.toDataURL({format: 'png', quality: 0.8});
-      });
-    };
-    reader.readAsDataURL(file);
-  });
-  //Selecting Font
-  var select = document.getElementById("fontf");
-  fonts.forEach(function(font) {
-    var option = document.createElement('option');
-    option.innerHTML = font;
-    option.value = font;
-    select.appendChild(option);
-  });
+Adjust();
 
-  document.getElementById('fontf').onchange = function() {
-    if (this.value !== 'Times New Roman') {
-      loadAndUse(this.value);
-    } else {
-      canvas.getActiveObject().set("fontFamily", this.value);
-      canvas.requestRenderAll();
+const texts = new Map();
+let activeText = null;
+
+const textOne = new fabric.Textbox("PutTextHere", {
+  fill: "blue",
+  fontStyle: "bold",
+  // fontFamily: "Times New Roman",
+  fontFamily: "Arial",
+
+  //fontFamily: 'montB'
+});
+
+function CreateText(e) {
+  document.getElementById("create-text").addEventListener("click", (e) => {
+    try {
+      const text = new fabric.Textbox("PutTextHere", {
+        fill: "blue",
+        fontStyle: "bold",
+        fontFamily: "Arial",
+      });
+
+      texts.set(text, text);
+      canvas.add(text).setActiveObject(text);
+      activeText = text;
+      text.onSelect = (e) => {
+        if (e) {
+          console.log("selected: ", text.fontFamily);
+          activeText = text;
+        }
+      };
+    } catch (error) {
+      console.log("error: ", error);
     }
-  };
+  });
+}
 
-  function loadAndUse(font) {
-    var myfont = new FontFaceObserver(font)
-    myfont.load()
-      .then(function() {
-        // when font is loaded, use it.
-        canvas.getActiveObject().set("fontFamily", font);
-        canvas.requestRenderAll();
-      }).catch(function(e) {
-        console.log(e)
-        alert('font loading failed ' + font);
-      });
-  }
-// Render the Text on Canvas
+CreateText();
+
+document.getElementById("file").addEventListener("change", function (e) {
+  const file = e.target.files[0];
+  const reader = new FileReader();
+  reader.onload = function (f) {
+    const data = f.target.result;
+    fabric.Image.fromURL(data, function (img) {
+      const oImg = img.set({ left: 40, top: 0, angle: 0, width: 520, height: 600 }).scale(0.4);
+      canvas.add(oImg).renderAll();
+      const a = canvas.setActiveObject(oImg);
+      const dataURL = canvas.toDataURL({ format: "png", quality: 0.8 });
+    });
+  };
+  reader.readAsDataURL(file);
+});
+
+//Selecting Font
+let select = document.getElementById("fontf");
+allFonts.forEach(function (font) {
+  let option = document.createElement("option");
+  option.innerHTML = font;
+  option.value = font;
+  select.appendChild(option);
+});
+
+document.getElementById("fontf").onchange = function () {
+  console.log("value: ", this.value);
+  loadAndUse(this.value);
+};
+
+function loadAndUse(font) {
+  activeText.fontFamily = font;
+  canvas.requestRenderAll();
+}
+
+// - track the current / selected text box
+// - get the get the selected font
+// - set the fontFamily of the current / selected text box
+// - re-render
